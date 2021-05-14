f1c3ef4d1a91245a99fa408a9afaba0429602b99
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2021-05-13 22:00:09 -0500

    aún no borra, pero ya casi A

--- js/Tarea.js
+++ js/Tarea.js
@@ -3,7 +3,7 @@ class Tarea{
         this.tarea = tarea
     }
 
-    render = () => {
+    renderToDo = () => {
 
         let component = document.createElement('div');
 
@@ -24,21 +24,149 @@ class Tarea{
         component.appendChild(moveUpBtn);
 
         borrarBtn.addEventListener('click', () => {
+            //debería borrar de toDo/
             const database = firebase.database();
-            database.ref('tareas/'+this.tarea).set(null);
+            database.ref('toDo/'+this.tarea.id).set(null);
             console.log(this.tarea.id);
             console.log(this.tarea.userTarea);
         });
 
         moveUpBtn.addEventListener('click', () =>{
-            const database = firebase.database();
-            database.ref('tareas/doing').push().set(this.tarea);
-            database.ref('tareas/toDo'+this.tarea.id).set(null);
+
+            let reference = database.ref('doing').push();
+
+            let tarea = {
+                id: reference.key,
+                userTarea: this.tarea.userTarea,
+            };
+            reference.set(tarea);
+
+            //borra en firebase
+            const databasee = firebase.database();
+            databasee.ref('toDo/'+this.tarea.id).set(null);
            
         });
 
         return component;
 
     }
+
+    renderDoing = () => {
+
+        let component = document.createElement('div');
+
+        let tareaCont = document.createElement('div');
+        tareaCont.className = 'tareaCont';
+        tareaCont.innerHTML = this.tarea.userTarea;
+
+        let moveUpBtn = document.createElement('button');
+        moveUpBtn.className = 'moveUpBtn';
+        moveUpBtn.innerHTML = (' > ');
+        //se añade el boton de regresar
+        let moveDownBtn = document.createElement('button');
+        moveDownBtn.className = 'moveDownBtn';
+        moveDownBtn.innerHTML = (' < ');
+
+        let borrarBtn = document.createElement('button');
+        borrarBtn.className = 'borrarBtn';
+        borrarBtn.innerHTML = (' X ');
+
+        component.appendChild(tareaCont);
+        component.appendChild(moveDownBtn);
+        component.appendChild(borrarBtn);
+        component.appendChild(moveUpBtn);
+
+        borrarBtn.addEventListener('click', () => {
+            //debería borrar de doing/
+            const database = firebase.database();
+            database.ref('doing/'+this.tarea.id).set(null);
+            console.log(this.tarea.id);
+            console.log(this.tarea.userTarea);
+        });
+
+        moveUpBtn.addEventListener('click', () =>{
+
+            let reference = database.ref('done').push();
+
+            let tarea = {
+                id: reference.key,
+                userTarea: this.tarea.userTarea,
+            };
+            reference.set(tarea);
+
+            //borra en firebase
+            const databasee = firebase.database();
+            databasee.ref('doing/'+this.tarea.id).set(null);
+           
+        });
+
+        moveDownBtn.addEventListener('click', () =>{
+            //para regresar la tarea a toDo/
+            let reference = database.ref('toDo').push();
+
+            let tarea = {
+                id: reference.key,
+                userTarea: this.tarea.userTarea,
+            };
+            reference.set(tarea);
+
+            //borra en firebase
+            const databasee = firebase.database();
+            databasee.ref('doing/'+this.tarea.id).set(null);
+           
+        });
+
+        return component;
+
+    }
+
+renderDone = () => {
+
+    let component = document.createElement('div');
+
+    let tareaCont = document.createElement('div');
+    tareaCont.className = 'tareaCont';
+    tareaCont.innerHTML = this.tarea.userTarea;
+
+    //se deja sólo el botón de regresar
+    let moveDownBtn = document.createElement('button');
+    moveDownBtn.className = 'moveDownBtn';
+    moveDownBtn.innerHTML = (' < ');
+
+    let borrarBtn = document.createElement('button');
+    borrarBtn.className = 'borrarBtn';
+    borrarBtn.innerHTML = (' X ');
+
+    component.appendChild(tareaCont);
+    component.appendChild(moveDownBtn);
+    component.appendChild(borrarBtn);
+
+    borrarBtn.addEventListener('click', () => {
+        //debería borrar de done/
+        const database = firebase.database();
+        database.ref('done/'+this.tarea.id).set(null);
+        console.log(this.tarea.id);
+        console.log(this.tarea.userTarea);
+    });
+
+    moveDownBtn.addEventListener('click', () =>{
+        //para regresar la tarea a toDo/
+        let reference = database.ref('doing').push();
+
+        let tarea = {
+            id: reference.key,
+            userTarea: this.tarea.userTarea,
+        };
+        reference.set(tarea);
+
+        //borra en firebase
+        const databasee = firebase.database();
+        databasee.ref('done/'+this.tarea.id).set(null);
+       
+    });
+
+    return component;
+
+}
 
 }
--- js/index.js
+++ js/index.js
@@ -8,45 +8,61 @@ const database = firebase.database();
 
 enviarTarea = () => {
 
-
+    //en caso de que el usuario no haya llenado la nueva tarea que quiere agregar
     if(tareaMsg.value == ''){
         alert('No ha escrito la tarea');
         return;
     }
 
-    let reference = database.ref('tareas/').push();
-
+    let reference = database.ref('toDo').push();
+// se crea el objeto de tarea
     let tarea = {
         id: reference.key,
         userTarea: tareaMsg.value,
     };
     console.log(tarea);
-    database.ref('tareas/toDo').push().set(tarea);
+    database.ref('toDo').push().set(tarea);
 
     tareaMsg.value = '';
 
 }
 
-database.ref('tareas/toDo').on('value', function(data){
+nuevaTareaBtn.addEventListener('click', enviarTarea);
+
+//para mostrar lo que está en toDo
+database.ref('toDo').on('value', function(data){
     toDoList.innerHTML = '';
     data.forEach(
         tarea => {
             let valor = tarea.val();
             let fila = new Tarea(valor);
-            toDoList.appendChild(fila.render());
+            toDoList.appendChild(fila.renderToDo());
         
     });
 });
 
-database.ref('tareas/doing').on('value', function(data){
+//para mostrar lo que está en doing
+
+database.ref('doing').on('value', function(data){
     doingList.innerHTML = '';
     data.forEach(
         tarea => {
             let valor = tarea.val();
             let fila = new Tarea(valor);
-            doingList.appendChild(fila.render());
+            doingList.appendChild(fila.renderDoing());
         
     });
 });
 
-nuevaTareaBtn.addEventListener('click', enviarTarea);
+//para mostrar lo que está en done
+
+database.ref('done').on('value', function(data){
+    doneList.innerHTML = '';
+    data.forEach(
+        tarea => {
+            let valor = tarea.val();
+            let fila = new Tarea(valor);
+            doneList.appendChild(fila.renderDone());
+        
+    });
+});
